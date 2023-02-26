98ca29ea5849155082c6df07712e7de526f1cafa
Khaled Allam <kaaoaa505@gmail.com>
2023-02-26 15:05:30 +0300

    run npm with port 8001 and php artisan with port 8002

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>MyBlog | My Awesome Blog</title>
+
+    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+</head>
+<body>
+    <header>
+        <nav class="navbar navbar-default navbar-fixed-top">
+          <div class="container">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                <div class="container-fluid">
+                  <a class="navbar-brand" href="#">BlogApp</a>
+                  <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                  </button>
+                  <div class="collapse navbar-collapse" id="navbarText">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                      <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                      </li>
+                      <li class="nav-item">
+                        <a class="nav-link" href="#">About</a>
+                      </li>
+                      <li class="nav-item">
+                        <a class="nav-link" href="#">Contact</a>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+              </nav>
+          </div><!-- /.container -->
+        </nav>
+    </header>
+
+    <div class="container">
+        <div class="row">
+
+            @yield('content')
+
+        </div>
+    </div>
+
+    <footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-8">
+                    <p class="copyright">&copy; {{ date('Y') }} Khaled Allam</p>
+                </div>
+                <div class="col-md-4">
+                    <nav>
+                        <ul class="social-icons">
+                            <li><a href="#" class="i fa fa-facebook"></a></li>
+                            <li><a href="#" class="i fa fa-twitter"></a></li>
+                            <li><a href="#" class="i fa fa-google-plus"></a></li>
+                            <li><a href="#" class="i fa fa-github"></a></li>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </footer>
+
+</body>
+</html>
